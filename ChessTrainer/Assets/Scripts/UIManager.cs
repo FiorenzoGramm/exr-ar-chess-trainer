09da1071ab6e1bbb0a3646a5c6824c9c2b2075fc
Fiorenzo <fiorenzo.gramm@students.fhnw.ch>
2022-05-29 18:05:22 +0200

    Adds e change theme button to the control panel which iterates through 3 colors.

--- ChessTrainer/Assets/Scripts/UIManager.cs
+++ ChessTrainer/Assets/Scripts/UIManager.cs
@@ -8,10 +8,15 @@ public class UIManager : MonoBehaviour
     public GameSelector gameSelectionMenu;
     public TextMeshPro gameInfoName;
     public GameHistoryController gameHistoryController;
-
+    public List<Material> Themes;
+    public Material MaterialToReplace;
+    public Material CurrentTheme;
+    public MeshRenderer Backplate;
+    private int CurrentThemeIndex;
     public void InitialiseUI(List<string> games)
     {
         gameSelectionMenu.LoadGames(games);
+        CurrentThemeIndex = Themes.IndexOf(CurrentTheme);
         gameInfoName.text = gameMangager.currentGame.name;
     }
 
@@ -34,5 +39,43 @@ public class UIManager : MonoBehaviour
     public void MoveUnDone()
     {
         gameHistoryController.RemoveMove();
+    }
+
+    public void ChangeTheme()
+    {
+        Debug.LogWarning("Triggered");
+
+        if (CurrentThemeIndex == Themes.Count - 1)
+        {
+            CurrentThemeIndex = 0;
+        }
+        else
+        {
+            ++CurrentThemeIndex;
+        }
+
+        Renderer[] allMeshes = GetComponentsInChildren<Renderer>();
+
+        foreach(Renderer currentMesh in allMeshes)
+        {
+            if (CurrentTheme.Equals(currentMesh.sharedMaterials[0]))
+            {
+                Debug.LogWarning(currentMesh.name);
+                currentMesh.material = Themes[CurrentThemeIndex];
+            }
+        }
+
+        allMeshes = gameSelectionMenu.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer currentMesh in allMeshes)
+        {
+            if (CurrentTheme.Equals(currentMesh.sharedMaterials[0]))
+            {
+                Debug.LogWarning(currentMesh.name);
+                currentMesh.material = Themes[CurrentThemeIndex];
+            }
+        }
+
+        CurrentTheme = Themes[CurrentThemeIndex];
     }
 }
